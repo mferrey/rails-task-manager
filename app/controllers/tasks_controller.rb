7ab03386a0f12c9eface653b8742a5debdc940ee
mathilde ferrey <mathildeferrey@yahoo.com>
2019-02-12 17:14:09 +0100

    2e exo ok style moche

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,46 @@
 class TasksController < ApplicationController
-   def index
+  def index
     @tasks = Task.all
   end
+
+  def show
+    taskfinder
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(title: params[:task][:title], details: params[:task][:details])
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    taskfinder
+
+  end
+
+  def update
+    taskfinder
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.delete
+    redirect_to tasks_path
+  end
+end
+
+private
+
+def taskfinder
+  @task = Task.find(params[:id])
+end
+
+def task_params
+  params.require(:task).permit(:title, :details, :completed)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  post 'tasks', to: 'tasks#create'
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
